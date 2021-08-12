f3c14186eaae7156273a9304edcc685f1fde3674
ungeschneuer <33438600+ungeschneuer@users.noreply.github.com>
2021-08-13 00:45:47 +0200

    Change in Twitter Posting

--- parser/twitter_creds.py
+++ parser/twitter_creds.py
@@ -16,21 +16,38 @@ def TwitterApi():
                 access_token_secret=os.environ.get('FIRST_ACCESS_TOKEN_SECRET'))
 
 
+def ContextTwitterApi():
+    return twitter.api.Api(consumer_key=os.environ.get('CONTEXT_CONSUMER_KEY'),
+                consumer_secret=os.environ.get('CONTEXT_CONSUMER_SECRET'),
+                access_token_key=os.environ.get('CONTEXT_ACCESS_TOKEN_KEY'),
+                access_token_secret=os.environ.get('CONTEXT_ACCESS_TOKEN_SECRET'))
+
+
 
 twitterAPI = TwitterApi()
+contextAPI = ContextTwitterApi()
 
 
 
 def tweet_word(word, sitzung, url):
     try:
         status = twitterAPI.PostUpdate(word)
-        twitterAPI.PostUpdate(
-            "Das Wort tauchte in der Sitzung {} zum ersten Mal auf. Das Protokoll findet man unter {}".format(
+        context_status = contextAPI .PostUpdate(
+            "@{} \"{}\" tauchte zum ersten Mal in Sitzung {} auf. Das Protokoll findet man unter {}".format(
+                status.user.screen_name,
+                word,
                 sitzung,
                 url),
             in_reply_to_status_id=status.id,
             verify_status_length=False)
+        
+        if context_status:
+            return True
+        else:
+            return False
+        
     except UnicodeDecodeError as e:
+        print(e)
         capture_exception(e)
         return False
     except twitter.TwitterError as e:
--- parser/twitter_queue.py
+++ parser/twitter_queue.py
@@ -20,26 +20,34 @@ def is_time_between(begin_time, end_time):
 
 def add_to_queue(word, sitzung, url):
 
+    print(word)
+
     r.hset(word, "word", word)
     r.hset(word, "sitzung", sitzung)
     r.hset(word, "url", url)
 
+    return True
+
 
 def tweet_queue():
     
-    tweetstop = r.get('tweetstop')
+    tweetstop = r.get('meta:tweetstop')
 
-    if is_time_between(time(8,30), time(17,30)) and tweetstop is None:
+    if tweetstop is None:
         key = r.randomkey()
         word = r.hget(key, "word").decode("utf-8") 
         sitzung = r.hget(key, "sitzung").decode("utf-8") 
         url = r.hget(key, "url").decode("utf-8") 
 
-        expireTime = 60*60*(round(random.randrange(0,16) * 0.12))
+        expireTime = 60*round(random.randrange(0,30))
 
         if tweet_word(word, sitzung, url):
-            r.set('tweetstop', 1 , ex=expireTime)
+            r.set('meta:tweetstop', 1 , ex=expireTime)
             r.delete(key)
+
+            return True
+
+
 
 
 
