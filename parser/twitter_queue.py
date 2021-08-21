1fe31c1ec1914c2290f527b1a9344dc79199a6a6
ungeschneuer <33438600+ungeschneuer@users.noreply.github.com>
2021-08-21 11:16:00 +0200

    Anpassung Rhytmus Tweets

--- parser/twitter_queue.py
+++ parser/twitter_queue.py
@@ -57,7 +57,7 @@ def tweet_queue():
             sitzung = r.hget(key, "sitzung").decode("utf-8") 
             url = r.hget(key, "url").decode("utf-8") 
 
-            expireTime = 60*round(random.randrange(1,15))
+            expireTime = 60*round(random.randrange(3,20))
 
             if tweet_word(word, sitzung, url):
                 sentry_sdk.capture_message("Tweet wurde gesendet.")
