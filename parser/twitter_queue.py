caacff9a03c167e2dacb3af57a07d43fb61622e1
ungeschneuer <33438600+ungeschneuer@users.noreply.github.com>
2021-08-22 21:12:38 +0200

    refactor

--- parser/twitter_queue.py
+++ parser/twitter_queue.py
@@ -1,3 +1,4 @@
+from sentry_sdk.api import capture_exception
 from database import twittRedis, pastRedis
 from twitter_creds import tweet_word
 import random
@@ -31,21 +32,37 @@ def tweet_queue():
     if tweetstop is None:
         key = twittRedis.randomkey()
         if key:
-            word = twittRedis.hget(key, "word").decode("utf-8") 
-            id = twittRedis.hget(key, "id").decode("utf-8") 
- 
-
-            expireTime = 60*round(random.randrange(3,20))
-
-            status_id = tweet_word(word, id)
-            if status_id:
-                sentry_sdk.capture_message("Tweet wurde gesendet.")
-                twittRedis.set('meta:tweetstop', 1 , ex=expireTime)
-                twittRedis.move(key['word'], pastRedis)
-                pastRedis.hset(key['word'], "id", status_id)
-
-                return True
+            return send_tweet(key)
     quit()
+
+def send_tweet(key):
+    word = twittRedis.hget(key, "word").decode("utf-8")
+    id = twittRedis.hget(key, "id").decode("utf-8") 
+
+
+    status_id = tweet_word(word, id)
+
+    if not status_id:
+        raise
+    
+    sentry_sdk.capture_message("Tweet wurde gesendet.")
+    return cleanup_db(key, status_id)
+
+def cleanup_db(key, status_id):
+
+    # Tweet Stopper eintstellen
+    expireTime = 60*round(random.randrange(3,20))
+    twittRedis.set('meta:tweetstop', 1 , ex=expireTime)
+
+    # Ins Archiv bewegen
+    try:
+        twittRedis.move(key['word'], pastRedis)
+        pastRedis.hset(key['word'], "tweet_id", status_id)
+        return True
+    except Exception as e:
+        capture_exception(e)
+        return False
+  
 
 
 
