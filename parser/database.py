675387ed589fca2ab022b9000f0b0481bd8f9736
ungeschneuer <33438600+ungeschneuer@users.noreply.github.com>
2021-08-23 13:37:56 +0200

    Optimierung Wortabgleich

--- parser/database.py
+++ parser/database.py
@@ -10,32 +10,51 @@ twittRedis = redis.StrictRedis(host='localhost', port=6379, db=1)
 pastRedis = redis.StrictRedis(host='localhost', port=6379, db=2)
 
 # Wort abgleichen und zur Datenbank hinzufügen
-def add_word(word, id):
-    pipe = r.pipeline()
+def compare_words(word):
     
-    # Checken ob Kleinschreibung, Großschreibung oder Plural schon existieren
-    pipe.hget("word:" + word, "word")
-    pipe.hget("word:" + word.lower(), "word")
-    pipe.hget("word:" + word.capitalize(), "word")
-
-    if word.endswith('s') or word.endswith('rn'):
-        pipe.hget("word:" + word[:-1], "word")
-
-    if word.endswith('’s') or word.endswith('in') or word.endswith('’n') or word.endswith('es'):
-        pipe.hget("word:" + word[:-2], "word")
-        
-
-    if all(v is None for v in pipe.execute()):
-        r.hset("word:" + word, "word", word)
-        r.hset("word:" + word, "id", id)
+    # Wenn genau das Wort existiert, skippen
+    if r.hget('word:' + word, 'word'):
         return True
+    
+    # Sonst Abfrage von verschiedenen Versionen
+    pipe = r.pipeline()
 
+    # Checken ob Kleinschreibung/Großschreibung
+    pipe.hget('word:' + word.lower(), 'word')
+    pipe.hget('word:' + word.capitalize(), 'word')
+
+
+    # Existiert es schon im Plural?
+    pipe.hget('word:' + word + 'er', 'word')
+    pipe.hget('word:' + word + 'n', 'word')
+    pipe.hget('word:' + word + 'en', 'word')
+    pipe.hget('word:' + word + 's', 'word')
+
+    # Fälle und Plural entfernen
+    if word.endswith(('s','n')):
+        pipe.hget('word:' + word[:-1], 'word')
+
+    if word.endswith(('’s', 'in', '’n')) or word[-2] == 'e':
+        pipe.hget('word:' + word[:-2], 'word')
+
+    return pipe.execute()
+
+
+
+
+def add_word(word, id):
+    
+    if all(v is None for v in compare_words(word)):
+        r.hset('word:' + word, 'word', word)
+        r.hset('word:' + word, 'id', id)
+        return True
 
     return False
 
 def add_to_queue(word, id):
 
-    twittRedis.hset(word, "word", word)
-    twittRedis.hset(word, "id", id)
+    twittRedis.hset(word, 'word', word)
+    twittRedis.hset(word, 'id', id)
+    
     return True
 
