ed84a58e96ec8c229fef02ce08197030635a76cf
ungeschneuer <33438600+ungeschneuer@users.noreply.github.com>
2022-01-13 09:37:30 +0100

    reformat

--- parser/plenar.py
+++ parser/plenar.py
@@ -27,25 +27,25 @@ def main():
     new_id = find_new_doc(old_id)
 
     if new_id:
+        
         xml_file = xml_parse.get(new_id)
+
+        if xml_file:
+            logging.info('Sitzung mit der ID ' + str(new_id) +  ' gefunden')
+            wordnum = process_woerter(xml_file, new_id)
+            if wordnum == 0:
+                logging.debug('Es wurde kein neues Wort hinzugefügt.')
+                exit   
+            else:
+                increase_current_id(new_id)
+            
+            logging.info("Es wurden " + str(wordnum) + " neue Wörter hinzugefügt.")
+
+        else:
+            logging.debug('Fehler im XML File.')
+
     else:
         logging.info('Keine neue Sitzung gefunden.')
-        exit
-
-
-    if xml_file:
-        logging.info('Sitzung mit der ID ' + str(new_id) +  ' gefunden')
-        wordnum = process_woerter(xml_file, new_id)
-        if wordnum == 0:
-            logging.debug('Es wurde kein neues Wort hinzugefügt.')
-            exit   
-        else:
-            increase_current_id(new_id)
-        
-        logging.info("Es wurden " + str(wordnum) + " neue Wörter hinzugefügt.")
-
-    else:
-        logging.debug('Fehler im XML File.')
     
     exit
 
