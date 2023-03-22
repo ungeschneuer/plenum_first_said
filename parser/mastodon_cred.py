8dd2de7ee16d9201b8f0eae8c09673638a8a1320
ungeschneuer <33438600+ungeschneuer@users.noreply.github.com>
2023-03-22 01:29:37 +0100

    endless loop fix

--- parser/mastodon_cred.py
+++ parser/mastodon_cred.py
@@ -24,122 +24,128 @@ def toot_word(word, keys, metadata):
     
     #Posts Word
     while True:
-        try: 
-            toot_status = MastodonAPI.toot(word)
-
-        except Exception as e:
-            logging.exception(e)
-            sleep(120)
-            patience += 1
-            continue
-        if patience > 50:
+        if patience > 10:
             logging.info('Maximale Versuche wurde überschritten.')
             return False
         else:
+            try: 
+                toot_status = MastodonAPI.toot(word)
+            except Exception as e:
+                logging.exception(e)
+                sleep(60)
+                patience += 1
+                continue
             break
 
     
     # Posts Context
     # Metada is information from OPTV
     if metadata:
+        # Max tries reset
+        patience = 0
         while True:
-            try:
-                context_status = MastodonKontextAPI.status_post("#{} tauchte zum ersten Mal im {} am {} auf. Es wurde von {} ({}) gesagt.\n\nVideo: {}".format(
-                        word,
-                        keys[b'titel'].decode('UTF-8'),
-                        keys[b'datum'].decode('UTF-8'),
-                        metadata['speaker'],
-                        metadata['party'],
-                        metadata['link']),
-                        in_reply_to_id = toot_status["id"])
-            except mastodon.MastodonNotFoundError as m:
-                logging.exception(m)
-                sleep(120)
-                patience += 1 
-                continue
-            except Exception as e:
-                logging.exception(e)
-                sleep(120)
-                patience += 1
-                continue
-            except:
-                logging.exception("Unbekannter Fehler")
-                sleep(120)
-                patience += 1
-                continue
-            if patience > 50:
+            if patience > 10:
                 logging.info('Maximale Versuche wurde überschritten.')
                 return False
             else:
+                try:
+                    context_status = MastodonKontextAPI.status_post("#{} tauchte zum ersten Mal im {} am {} auf. Es wurde von {} ({}) gesagt.\n\nVideo: {}".format(
+                            word,
+                            keys[b'titel'].decode('UTF-8'),
+                            keys[b'datum'].decode('UTF-8'),
+                            metadata['speaker'],
+                            metadata['party'],
+                            metadata['link']),
+                            in_reply_to_id = toot_status["id"])
+                except mastodon.MastodonNotFoundError as m:
+                    logging.exception(m)
+                    sleep(60)
+                    patience += 1 
+                    continue
+                except Exception as e:
+                    logging.exception(e)
+                    sleep(60)
+                    patience += 1
+                    continue
+                except:
+                    logging.exception("Unbekannter Fehler")
+                    sleep(60)
+                    patience += 1
+                    continue
                 break
 
-
+        # Max tries reset
+        patience = 0
 
         while True:
-            try:
-                second_context_status = MastodonKontextAPI.status_post(
-                    "Das {} findet sich als PDF unter {}".format(
-                        context_status.user.screen_name,
-                        keys[b'titel'].decode('UTF-8'),
-                        keys[b'pdf_url'].decode('UTF-8')),
-                    in_reply_to_status_id=context_status.id)
-            except mastodon.MastodonNotFoundError as m:
-                logging.exception(m)
-                sleep(120)
-                patience += 1
-                continue
-            except AttributeError as e:
-                logging.exception(e)
-                sleep(120)
-                patience += 1
-                continue
 
-            except Exception as e:
-                logging.exception(e)
-                sleep(120)
-                patience += 1
-                continue
-            except:
-                logging.exception("Unbekannter Fehler")
-                sleep(120)
-                patience += 1
-                continue
-            if patience > 50:
+            if patience > 10:
                 logging.info('Maximale Versuche wurde überschritten.')
                 return False
             else:
+                try:
+                    second_context_status = MastodonKontextAPI.status_post(
+                        "Das {} findet sich als PDF unter {}".format(
+                            context_status.user.screen_name,
+                            keys[b'titel'].decode('UTF-8'),
+                            keys[b'pdf_url'].decode('UTF-8')),
+                        in_reply_to_status_id=context_status.id)
+                except mastodon.MastodonNotFoundError as m:
+                    logging.exception(m)
+                    sleep(60)
+                    patience += 1
+                    continue
+                except AttributeError as e:
+                    logging.exception(e)
+                    logging.info(context_status)
+                    sleep(60)
+                    patience += 1
+                    continue
+                except Exception as e:
+                    logging.exception(e)
+                    sleep(60)
+                    patience += 1
+                    continue
+                except:
+                    logging.exception("Unbekannter Fehler")
+                    sleep(60)
+                    patience += 1
+                    continue
                 break
 
 
 
     else:
+        # Max tries reset
+        patience = 0
         while True:
-            try:     
-                context_status = MastodonKontextAPI.status_post("#{} tauchte zum ersten Mal im {} am {} auf. Das Protokoll findet sich unter {}".format(
-                    word,
-                    keys[b'titel'].decode('UTF-8'),
-                    keys[b'datum'].decode('UTF-8'),
-                    keys[b'pdf_url'].decode('UTF-8')),
-                    in_reply_to_id = toot_status["id"])
-            except mastodon.MastodonNotFoundError as m:
-                logging.exception("Unbekannter Fehler")
-                sleep(120)
-                patience += 1 
-                continue
-            except Exception as e:
-                logging.exception(e)
-                sleep(120)
-                patience += 1
-                continue
-            except:
-                logging.exception("Unbekannter Fehler")
-                sleep(120)
-                patience += 1
-                continue
-            if patience > 50:
+
+            if patience > 10:
                 logging.info('Maximale Versuche wurde überschritten.')
                 return False
             else:
+                try:     
+                    context_status = MastodonKontextAPI.status_post("#{} tauchte zum ersten Mal im {} am {} auf. Das Protokoll findet sich unter {}".format(
+                        word,
+                        keys[b'titel'].decode('UTF-8'),
+                        keys[b'datum'].decode('UTF-8'),
+                        keys[b'pdf_url'].decode('UTF-8')),
+                        in_reply_to_id = toot_status["id"])
+                except mastodon.MastodonNotFoundError as m:
+                    logging.exception("Unbekannter Fehler")
+                    sleep(60)
+                    patience += 1 
+                    continue
+                except Exception as e:
+                    logging.exception(e)
+                    sleep(60)
+                    patience += 1
+                    continue
+                except:
+                    logging.exception("Unbekannter Fehler")
+                    sleep(60)
+                    patience += 1
+                    continue
                 break
 
 
